320522bf009e4b53eb38e4f0fefa0d0cea05f23b
Kashif Usman <usmankashif544@gmail.com>
2023-01-23 17:25:47 +0500

    adding dark mode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,69 +1,83 @@
-import React from 'react'
-import './navbar.css'
-import logo from '../../assets/logo.png'
+import React from "react";
+import "./navbar.css";
+import logo from "../../assets/logo.png";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
-import Resume from '../../assets/Kashif Usman.pdf'
 
 function Navbar() {
-    const [toggleMenu, setToggleMenu] = React.useState(false);
-    return (
-        <div className='portfolio__navbar'>
-            <div className='portfolio__navbar-links'>
-              <div className='portfolio__navbar-logo'>
-                <img src={logo} alt='portfolio logo'/>
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [isDarkMode, setDarkMode] = React.useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode(!isDarkMode);
+  }
+
+  return (
+    <div className="portfolio__navbar">
+      <div className="portfolio__navbar-links">
+        <div className="portfolio__navbar-logo">
+          <img src={logo} alt="portfolio logo" />
+        </div>
+        <div className="portfolio__navbar-links_container">
+          <p>
+            <a href="#home">Home </a>
+          </p>
+          <p>
+            <a href="#about"> About </a>
+          </p>
+          <p>
+            <a href="#services">Services</a>
+          </p>
+          <p>
+            <a href="#projects"> Projects</a>
+          </p>
+          <p>
+            <a href="#contact">Contact</a>
+          </p>
+        </div>
+        <div className={`${isDarkMode ? "dark-mode" : "light-mode"}`}>
+          <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+        </div>
+        <div className="portfolio__navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              color="#fff"
+              size={27}
+              onClick={() => setToggleMenu(true)}
+            />
+          )}
+          {toggleMenu && (
+            <div className="portfolio__navbar-menu_container scale-up-center">
+              <div className="portfolio__navbar-menu_container-links">
+                <p>
+                  <a href="#home">Home </a>
+                </p>
+                <p>
+                  <a href="#projects"> Projects</a>
+                </p>
+                <p>
+                  <a href="#about"> About </a>
+                </p>
+                <p>
+                  <a href="#contact">Contact</a>
+                </p>
               </div>
-              <div className='portfolio__navbar-links_container'>
-                    <p><a href='#home'>Home </a></p>
-                    <p><a href='#about'> About </a></p>
-                    <p><a href='#services'>Services</a></p>
-                    <p><a href='#projects'> Projects</a></p>
-                    <p><a href='#contact'>Contact</a></p>
-              </div>
-              <div className='portfolio__navbar-cv-btn'>
-                  <button type='button'>
-                    <a href={Resume} download>
-                      Download CV
-                    </a>
-                  </button>
-              </div>
-              <div className='portfolio__navbar-menu'>
-                  {
-                    toggleMenu ? (
-                    <RiCloseLine
-                    color="#fff"
-                    size={27}
-                    onClick={() => setToggleMenu(false)}
-                  /> ): (
-                    <RiMenu3Line
-                    color="#fff"
-                    size={27}
-                    onClick={() => setToggleMenu(true)}
-                  />
-                  )
-                  }
-                  {
-                    toggleMenu && (
-                      <div className='portfolio__navbar-menu_container scale-up-center'>
-                        <div className='portfolio__navbar-menu_container-links'>
-                        <p><a href='#home'>Home </a></p>
-                        <p><a href='#projects'> Projects</a></p>
-                        <p><a href='#about'> About </a></p>
-                        <p><a href='#contact'>Contact</a></p>
-                        </div>
-                        <div className='portfolio__navbar-menu_cvbutton'>
-                        <p>
-                            <a href={Resume} download>
-                              Download CV
-                            </a>
-                          </p>
-                        </div>
-                      </div>
-                    )
-                  }
+              <div className="portfolio__navbar-menu_cvbutton">
+                <button type="button" onClick={toggleDarkMode}>
+                  Change Mode
+                </button>
               </div>
             </div>
+          )}
         </div>
-      )
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
